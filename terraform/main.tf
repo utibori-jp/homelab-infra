179fad9fb9f623060e991b4c85bd4fc98739cad7
utibori-jp <kouki.utibori@gmail.com>
2025-12-10 23:41:57 +0900

    feat(terraform): add k3s worker nodes configuration
    Added proxmox_vm_qemu resource for k3s workers in main.tf.
    Used 'count' parameter to enable scalable node provisioning.
    Configured cloud-init for IP assignment and user settings.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,50 @@
 resource "proxmox_vm_qemu" "k8s_master" {
-    name = "k8s-master"
-    target_node = "pve"
-    clone = "ubuntu-2204-cloudinit-template"
-    cores = 2
-    memory = 4096
+  name        = "k8s-master"
+  target_node = "pve"
+  clone       = "ubuntu-2204-cloudinit-template"
+  cores       = 2
+  memory      = 4096
 
-    ipconfig = "ip=192.168.1.10/24, gw=192.168.1.1"
-    sshkeys = file("~/.ssh/id_rsa.pub")
+  ipconfig = "ip=192.168.1.10/24, gw=192.168.1.1"
+  sshkeys  = file("~/.ssh/id_rsa.pub")
+}
+
+resource "proxmox_vm_qemu" "k3s_workers" {
+  count = 2
+
+  name = "k3s-worker-0${count.index + 1}"
+
+  target_node = "pve"
+
+  clone = "k8s-base-template"
+
+  # 基本設定
+  cores    = 2
+  sockets  = 1
+  memory   = 4096
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+
+  disk {
+    slot    = 0
+    size    = "32G"
+    type    = "scsi"
+    storage = "local-lvm"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  os_type = "cloud-init"
+
+  sshkeys = <<EOF
+  // TODO: set sshkeys
+  EOF
+
+  ciuser     = "ubuntu"
+  cipassword = "password"
+
+  ipconfig0 = "ip=192.168.0.2${count.index + 1}/24,gw=192.168.0.1"
 }
