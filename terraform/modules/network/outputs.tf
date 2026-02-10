e44b44dc641a7c88438579ad82ca1f2eb705c4b0
utibori-jp <kouki.utibori@gmail.com>
2026-02-10 21:52:09 +0900

    refactor(network): expose vnet name and zone outputs

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,3 +1,22 @@
+################################################################################
+# SDN VNet
+################################################################################
+
 output "vnet_id" {
-  value = proxmox_virtual_environment_sdn_vnet.k8s_vnet.id
+  description = "The unique identifier of the SDN VNet resource."
+  value       = proxmox_virtual_environment_sdn_vnet.k8s_vnet.id
+}
+
+output "vnet_zone" {
+  description = "The SDN Zone ID where this VNet belongs."
+  value       = proxmox_virtual_environment_sdn_vnet.k8s_vnet.zone
+}
+
+################################################################################
+# Network Configuration Details
+################################################################################
+
+output "vnet_tag" {
+  description = "The VLAN tag associated with the VNet (if configured)."
+  value       = try(proxmox_virtual_environment_sdn_vnet.k8s_vnet.tag, null)
 }
